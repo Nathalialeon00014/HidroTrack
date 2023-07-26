e80553931593270e4c250b7e3bcac9b276dbee66
Nathizleon <lindaleoncita@outlook.com>
2023-07-26 17:46:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hidrotrack/water_consumption.java
@@ -0,0 +1,14 @@
+package com.example.hidrotrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class water_consumption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_consumption);
+    }
+}
